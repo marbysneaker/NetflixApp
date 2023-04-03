f7d0e090d18a8edb26e2ab479500d8cfe8775c66
marbypangilinan <marbysneaker@gmail.com>
2023-04-03 03:01:40 -0700

    cloned in macbook

--- my-project/src/components/Main.jsx
+++ my-project/src/components/Main.jsx
@@ -16,13 +16,18 @@ const Main = () => {
     },[])
     console.log(movie)
   return (
-    <div className="w-full h-[500px] text-white">
+    <div className="w-full h-[550px] text-white">
     
         <div className="w-full h-full">
-
+            <div className='absolute w-full h-[550px] bg-gradient-to-r from-black'></div>
             <img className='w-full h-full' src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`} alt={movie?.title} />
             {movie?.title}{movie?.name}
 
+            <div className='absolute w-full top-[20%] p-4 md:p-8'>
+                <button className='border bg-gray-300 text-black border-gray-300 py2 px5'>Play</button>
+                <button className='border text-white border-gray-300 py2 px5 ml-4'>Watch Later</button>
+                
+            </div>
         </div>
     
     
