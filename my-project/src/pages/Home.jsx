90f5aa87794ad047cf8c497781a19e0a76e8bd00
marbypangilinan <marbysneaker@gmail.com>
2023-04-04 00:04:24 -0700

    finishing the design

--- my-project/src/pages/Home.jsx
+++ my-project/src/pages/Home.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import Main from '../components/Main'
+import Row from '../components/Row'
+import request from '../Request'
+
 
 
 const Home = () => {
   return (
     <div>
         <Main/>
+        <Row title = 'Up coming' fetchUrl = {request.requestUpcoming}/>
+        <Row title = 'Top Rated' fetchUrl = {request.requestTopRated}/>
+        <Row title = 'Trending' fetchUrl = {request.requestTrending}/>
+        <Row title = 'Up coming' fetchUrl = {request.requestUpcoming}/>
+        <Row title = 'Up coming' fetchUrl = {request.requestUpcoming}/>
     </div>
   )
 }
