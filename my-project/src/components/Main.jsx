5cf8c01d2957b740594f956de91cce09eb886582
Marcelino Pangilinan <marbysneaker@gmail.com>
2023-04-06 23:35:56 -0700

    what

--- my-project/src/components/Main.jsx
+++ my-project/src/components/Main.jsx
@@ -16,13 +16,15 @@ const Main = () => {
     },[])
     console.log(movie)
   return (
-    <div className="w-full h-[500px] text-white">
+    <div className="w-full h-[550px] text-white">
     
         <div className="w-full h-full">
-
-            <img className='w-full h-full' src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`} alt={movie?.title} />
-            {movie?.title}{movie?.name}
-
+            <div className="absolute w-full h-[550px] bg-gradient-to-r from-black"></div>
+            <img className='w-full h-full object-cover' src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`} alt={movie?.title} />
+            <div className='aboslute w-full top-'>
+                <button className='border bg-gray-300 text-black border-gray-300 py-2 px-5'>Play</button>
+                <button className='border text-white border-gray-300 py-2 px-5 ml-4'>watch Later</button>
+            </div>
         </div>
     
     
