70b7dba10b264a02dd591af58dded790b6a28c9d
Marcelino Pangilinan <marbysneaker@gmail.com>
2023-04-07 01:58:04 -0700

    finished building app

--- my-project/src/components/Movie.jsx
+++ my-project/src/components/Movie.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
 import { FaHeart, FaRegHeart} from 'react-icons/fa'
+import { UserAuth } from '../context/AuthContext';
 
 import {useState } from 'react'
+import { db } from '../firebase'
+import{arrayUnion, doc , updateDoc} from 'firebase/firestore'
 
 const Movie = ({item}) => {
   const [like, setLike] = useState(false)
+  const[saved, setSaved] = useState(false)
+  const{user} = UserAuth()
+  const movieId = doc(db, 'users', `${user?.email}` )
+  const saveShow = async () => {
+    if(user?.email){
+      setLike(!like)
+      setSaved(true)
+      await updateDoc(movieId,{
+        saveShows: arrayUnion({
+          id: item.id,
+          title: item.title,
+          img: item.backdrop_path
+        })
+      })
+    }
+    else{
+      alert('Please log in to save movies')
+    }
+  }
 
   return (
     <div className='w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2'>
@@ -13,7 +35,7 @@ const Movie = ({item}) => {
                     <p className='white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center'>
                         {item?.title}
                      </p>
-                     <p>
+                     <p onClick={saveShow}>
                         {like? <FaHeart className='absolute top-4 left-4 text-gray-300'/> : <FaRegHeart className='absolute top-4 left-4 text-gray-300'/>}
                      </p>
 
--- my-project/src/pages/Account.jsx
+++ my-project/src/pages/Account.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import SavedShows from '../components/SavedShows'
 
 const Account = () => {
   return (
-    <div>
-      Account
+    <>
+    <div className='w-full text-white'>
+        <img className='w-full h-[400px] object-cover' src="https://assets.nflxext.com/ffe/siteui/vlv3/2e884ce2-da1c-4501-ab9a-10c534d30975/96955d63-0bb3-452c-a226-67c595f275b9/US-en-20230327-popsignuptwoweeks-perspective_alpha_website_large.jpg" alt="/" />
+        <div className='bg-black/60 fixed top-0 left-0 h-[550px] w-full'></div>
+        <div className='absolute top-[20%] p-4 md:p-8'>
+          <h1 className='text-3xl md:text-5xl font-bold'>My Shows</h1>
+        </div>
     </div>
+    <SavedShows/>
+    </>
   )
 }
 
